9073b4686c75cef86128f0343661b13c0c019db0
Walter Mercado <Walter.Mercado@jalasoft.com>
2017-01-26 15:55:15 -0400

    Adding a Get REST call

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'JSON'
 
 get '/' do
   erb :hello_world
+end
+
+get '/greeting' do
+  content_type :json # Content-Type: application/json;charset=utf-8
+
+  # Use to_json to generate JSON based on the Ruby hash
+  {greeting: 'Hello World!'}.to_json
 end
